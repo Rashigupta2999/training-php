90882fd2dfffc1459f817d1f972f436fd481868d
Rashigupta2999 <rashi.gupta@webtechstreet.com>
2023-02-02 14:04:22 +0530

    Read about difference in echo and print.

---
+++ echoprint.php
@@ -0,0 +1,10 @@
+<?php
+    echo "Hii This is the example of echo <br>";
+    echo "Hii ", "echo ", "accept ", "multiple parameter..";
+    echo "<h1>It accept css under string as well<h1>";
+    print "<h3>It is same as echo<h3> <br>";
+    print "<h2>it does not support multiple parameter<h2>";
+    //print "It " ,"accept ", "css ";
+?>
+
+
